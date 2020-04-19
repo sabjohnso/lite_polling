c7aacc89ab2a8d0f6fcfb8d0ed9e23b7bdbd59e8
Samuel B. Johnson <sabjonso@yahoo.com>
2020-04-19 12:47:22 -0500

    Added documentation struct (not actual documentation)

---
+++ doc/source/index.rst
@@ -0,0 +1,20 @@
+.. lite_polling documentation master file, created by
+   sphinx-quickstart on Sat Apr 18 12:33:41 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to lite_polling's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import setuptools
 
+import sphinx
+from sphinx.setup_command import BuildDoc
+cmdclass = {'build_sphinx': BuildDoc}
+
 with open('README.rst', 'r') as fh:
     long_description = fh.read()
 
@@ -7,17 +11,22 @@ setuptools.setup(
     name="lite_polling",
     version="0.1.0",
     author="Samuel B. Johnson",
-    author_email="sabjohnso@yahoo.com",
+    author_email="sabjohnso.dev@gmail.com",
     long_description = long_description,
     long_description_content_type = "text/x-rst",
     url = "https://github.com/sbjohnso/lite_polling",
+    license = "MIT",
     packages = setuptools.find_packages(),
+    cmdclass = cmdclass,
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Idependent"
     ],
-    python_requires = ">=3.4"
+    python_requires = ">=3.4",
+    command_options = {
+        'build_sphinx' : {
+            'source_dir' :  ('setup.py', 'doc/source')}}
 )
 
 
